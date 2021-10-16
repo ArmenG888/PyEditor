b52a0faf3fbb13eb4329aede7e679469713c645f
Armen._.G <52843550+ArmenG888@users.noreply.github.com>
2021-10-16 14:19:16 -0700

    Create ui_pytexteditor.py

---
+++ PyEditor/ui_pytexteditor.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'pytexteditorkXMxXv.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(804, 592)
+        MainWindow.setStyleSheet(u"background-color:rgb(71, 71, 71);")
+        self.actionNew_File = QAction(MainWindow)
+        self.actionNew_File.setObjectName(u"actionNew_File")
+        self.actionOpen_File = QAction(MainWindow)
+        self.actionOpen_File.setObjectName(u"actionOpen_File")
+        self.actionSave = QAction(MainWindow)
+        self.actionSave.setObjectName(u"actionSave")
+        self.actionSave_As = QAction(MainWindow)
+        self.actionSave_As.setObjectName(u"actionSave_As")
+        self.actionUndo = QAction(MainWindow)
+        self.actionUndo.setObjectName(u"actionUndo")
+        self.actionRedo = QAction(MainWindow)
+        self.actionRedo.setObjectName(u"actionRedo")
+        self.actionLicenses = QAction(MainWindow)
+        self.actionLicenses.setObjectName(u"actionLicenses")
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.lineEdit = QLineEdit(self.centralwidget)
+        self.lineEdit.setObjectName(u"lineEdit")
+        self.lineEdit.setGeometry(QRect(0, 0, 801, 561))
+        self.lineEdit.setLayoutDirection(Qt.LeftToRight)
+        self.lineEdit.setInputMethodHints(Qt.ImhNone)
+        self.lineEdit.setFrame(True)
+        self.lineEdit.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignTop)
+        self.lineEdit.setDragEnabled(False)
+        self.lineEdit.setReadOnly(False)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.status = QStatusBar(MainWindow)
+        self.status.setObjectName(u"status")
+        MainWindow.setStatusBar(self.status)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 804, 21))
+        self.menubar.setStyleSheet(u"color:rgb(255,255,255);")
+        self.menuFile = QMenu(self.menubar)
+        self.menuFile.setObjectName(u"menuFile")
+        self.menuEdit = QMenu(self.menubar)
+        self.menuEdit.setObjectName(u"menuEdit")
+        self.menuSettings = QMenu(self.menubar)
+        self.menuSettings.setObjectName(u"menuSettings")
+        self.menuAbout = QMenu(self.menubar)
+        self.menuAbout.setObjectName(u"menuAbout")
+        MainWindow.setMenuBar(self.menubar)
+
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menubar.addAction(self.menuEdit.menuAction())
+        self.menubar.addAction(self.menuSettings.menuAction())
+        self.menubar.addAction(self.menuAbout.menuAction())
+        self.menuFile.addAction(self.actionNew_File)
+        self.menuFile.addAction(self.actionOpen_File)
+        self.menuFile.addAction(self.actionSave)
+        self.menuFile.addAction(self.actionSave_As)
+        self.menuEdit.addAction(self.actionUndo)
+        self.menuEdit.addAction(self.actionRedo)
+        self.menuAbout.addAction(self.actionLicenses)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.actionNew_File.setText(QCoreApplication.translate("MainWindow", u"New File", None))
+        self.actionOpen_File.setText(QCoreApplication.translate("MainWindow", u"Open File", None))
+        self.actionSave.setText(QCoreApplication.translate("MainWindow", u"Save", None))
+        self.actionSave_As.setText(QCoreApplication.translate("MainWindow", u"Save As", None))
+        self.actionUndo.setText(QCoreApplication.translate("MainWindow", u"Undo", None))
+        self.actionRedo.setText(QCoreApplication.translate("MainWindow", u"Redo", None))
+        self.actionLicenses.setText(QCoreApplication.translate("MainWindow", u"License", None))
+        self.lineEdit.setText("")
+        self.menuFile.setTitle(QCoreApplication.translate("MainWindow", u"File", None))
+        self.menuEdit.setTitle(QCoreApplication.translate("MainWindow", u"Edit", None))
+        self.menuSettings.setTitle(QCoreApplication.translate("MainWindow", u"Settings", None))
+        self.menuAbout.setTitle(QCoreApplication.translate("MainWindow", u"About", None))
+    # retranslateUi
+
